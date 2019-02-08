4fce4e01cfdbd1679515e07c3690a2aa9f09588e
Siddharth Vaish <sidvaish97@gmail.com>
2019-02-08 22:24:37 +0530

    changes in directionactivity

--- app/src/main/java/com/hackathon/offlinemaps/DirectionActivity.java
+++ app/src/main/java/com/hackathon/offlinemaps/DirectionActivity.java
@@ -3,12 +3,16 @@ package com.hackathon.offlinemaps;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
 
 import com.hackathon.offlinemaps.RetrofitUtils.ApiClient;
 import com.hackathon.offlinemaps.RetrofitUtils.ApiInterface;
 import com.hackathon.offlinemaps.RetrofitUtils.ModelAllResults;
+import com.hackathon.offlinemaps.RetrofitUtils.ModelSteps;
+import com.hackathon.offlinemaps.SmsUtils.SmsHelper;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 import retrofit2.Call;
@@ -18,7 +22,8 @@ import retrofit2.Response;
 public class DirectionActivity extends AppCompatActivity {
     
     private static final String API_KEY = "AIzaSyBIZgWCuEGpS7mkV7Ozx4HzwVjKE35tuL0";
-    
+    private static final String TAG = DirectionActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,13 +38,39 @@ public class DirectionActivity extends AppCompatActivity {
                 ApiClient.getClient().create(ApiInterface.class);
         
         
-        Call<ModelAllResults> call = apiService.getAllDirectionData("iiitm campus", "Hazira gwl", API_KEY);
+        Call<ModelAllResults> call = apiService.getAllDirectionData("iiitm campus", "dd mall", API_KEY);
         Log.e("TestActivity", "Reached getPlaces ");
         call.enqueue(new Callback<ModelAllResults>() {
             @Override
             public void onResponse(@NonNull Call<ModelAllResults> call, Response<ModelAllResults> response) {
                 if (response.body() != null) {
                     Log.e("TestActivity", String.valueOf(response.body().getRoute().get(0).getLegs().get(0).getSteps().get(0).getHtmlInstructions()));
+
+                    ArrayList<ModelSteps> steps = response.body().getRoute().get(0).getLegs().get(0).getSteps();
+                    ArrayList<String> finalMessage = new ArrayList<>();
+
+                    ArrayList<String> htmlInstruction = new ArrayList<>();
+                    for ( ModelSteps step: steps)
+                    {
+                        htmlInstruction.add(step.getHtmlInstructions());
+                        Log.e(TAG,  (Html.fromHtml(Html.fromHtml(step.getHtmlInstructions()).toString()))+"\n\n");
+                        Log.e(TAG, String.valueOf(step.getDistance().get("text"))+"\n");
+
+                        String htmlText = String.valueOf(Html.fromHtml(Html.fromHtml(step.getHtmlInstructions()).toString()));
+                        String distance = String.valueOf(step.getDistance().get("text"));
+
+                        String finalString = htmlText+" for "+distance+" then";
+                        finalMessage.add(finalString);
+
+                    }
+
+                    for ( String message : finalMessage)
+                    {
+                        SmsHelper.sendDebugSms(String.valueOf("8765114937"), SmsHelper.SMS_CONDITION + " "+message);
+                    }
+
+                    Log.e(TAG, "The length of steps is "+steps.size());
+
                 }
             }
             @Override
