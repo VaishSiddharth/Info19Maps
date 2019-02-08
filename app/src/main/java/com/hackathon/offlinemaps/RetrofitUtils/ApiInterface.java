3186ed93dd8b233c62a205a1a71e759e09578a70
Siddharth Vaish <sidvaish97@gmail.com>
2019-02-09 02:53:21 +0530

    AutoComplete added

--- app/src/main/java/com/hackathon/offlinemaps/RetrofitUtils/ApiInterface.java
+++ app/src/main/java/com/hackathon/offlinemaps/RetrofitUtils/ApiInterface.java
@@ -13,5 +13,7 @@ public interface ApiInterface {
     @GET("maps/api/directions/json")
     Call<ModelAllResults> getAllDirectionData(@Query("origin") String startLocation, @Query("destination")
             String endLocation , @Query("key") String key);
-    
+    @GET("/maps/api/place/autocomplete/json")
+    Call<ModelAllResultsAutoPlaces> getAllAutoCompleteData(@Query("input")String place,@Query("key") String key);
+
 }
