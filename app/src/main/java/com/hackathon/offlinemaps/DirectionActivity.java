8481622180fb852e42ba30b29931cd6355ac9695
veloxIsTesting <aviharsh234>
2019-02-09 11:06:57 +0530

    initial_commit

--- app/src/main/java/com/hackathon/offlinemaps/DirectionActivity.java
+++ app/src/main/java/com/hackathon/offlinemaps/DirectionActivity.java
@@ -1,6 +1,7 @@
 package com.hackathon.offlinemaps;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,6 +15,7 @@ import com.hackathon.offlinemaps.RetrofitUtils.ModelAllResults;
 import com.hackathon.offlinemaps.RetrofitUtils.ModelAllResultsAutoPlaces;
 import com.hackathon.offlinemaps.RetrofitUtils.ModelDescription;
 import com.hackathon.offlinemaps.RetrofitUtils.ModelSteps;
+import com.hackathon.offlinemaps.SmsUtils.CommonConst;
 import com.hackathon.offlinemaps.SmsUtils.SmsHelper;
 
 import java.lang.reflect.Array;
@@ -29,7 +31,7 @@ public class DirectionActivity extends AppCompatActivity {
     
     private static final String API_KEY = "AIzaSyBIZgWCuEGpS7mkV7Ozx4HzwVjKE35tuL0";
     private static final String TAG = DirectionActivity.class.getSimpleName();
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,57 +42,55 @@ public class DirectionActivity extends AppCompatActivity {
     }
     
     private void getDirections() {
-
-
+        
         final ApiInterface apiService =
                 ApiClient.getClient().create(ApiInterface.class);
         Intent intent = getIntent();
-        String message = intent.getStringExtra("smsbody");
-        Log.e(TAG,message);
-
+        final String message = intent.getStringExtra("smsbody");
+        Log.e(TAG, message);
+        
         //code to extract start and end location
         List<String> tokens = new ArrayList<String>();
-        int startindex=0;
-        int endindex=0;
-        String startloc="";
-        String endloc="";
+        int startindex = 0;
+        int endindex = 0;
+        String startloc = "";
+        String endloc = "";
         StringTokenizer st = new StringTokenizer(message);
-
+        
         //("---- Split by space ------");
         while (st.hasMoreElements()) {
             tokens.add(st.nextElement().toString());
         }
-        for(int i=0;i<tokens.size();i++){
-            if(tokens.get(i).equalsIgnoreCase("START"))
-                startindex=i;
-            if(tokens.get(i).equalsIgnoreCase("END"))
-                endindex=i;
+        for (int i = 0; i < tokens.size(); i++) {
+            if (tokens.get(i).equalsIgnoreCase("START"))
+                startindex = i;
+            if (tokens.get(i).equalsIgnoreCase("END"))
+                endindex = i;
         }
-        for (int i=startindex+1;i<endindex;i++) {
-            startloc=startloc+tokens.get(i)+" ";
+        for (int i = startindex + 1; i < endindex; i++) {
+            startloc = startloc + tokens.get(i) + " ";
         }
-        for (int i=endindex+1;i<tokens.size();i++) {
-            endloc=endloc+tokens.get(i)+" ";
+        for (int i = endindex + 1; i < tokens.size(); i++) {
+            endloc = endloc + tokens.get(i) + " ";
         }
         //code of extract ENDED
-
-
+        
         //code for autocomplete places
-        Call<ModelAllResultsAutoPlaces> callplace=apiService.getAllAutoCompleteData(startloc,API_KEY);
+        Call<ModelAllResultsAutoPlaces> callplace = apiService.getAllAutoCompleteData(startloc, API_KEY);
         final String finalStartloc = startloc;
-        final String finalEndloc = endloc;
+        endloc = endloc.replace("<#>", "");
+        final String finalEndloc = endloc.replace(/*Replace hash */ "1VX4II6FyO7", "");
         callplace.enqueue(new Callback<ModelAllResultsAutoPlaces>() {
             @Override
             public void onResponse(Call<ModelAllResultsAutoPlaces> call2, Response<ModelAllResultsAutoPlaces> response) {
-                ArrayList<ModelDescription> predictions=response.body().getPredictions();
-                ArrayList<String> predict=new ArrayList<>();
-                for(ModelDescription description:predictions)
-                {
+                ArrayList<ModelDescription> predictions = response.body().getPredictions();
+                ArrayList<String> predict = new ArrayList<>();
+                for (ModelDescription description : predictions) {
                     predict.add(description.getDescription());
                 }
-                if(predict.size()==1){
+                if (predict.size() == 1) {
                     //add code for directions send
-                    Toast.makeText(getApplicationContext(), finalStartloc +"    "+ finalEndloc,Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), finalStartloc + "    " + finalEndloc, Toast.LENGTH_LONG).show();
                     Call<ModelAllResults> call = apiService.getAllDirectionData(finalStartloc, finalEndloc, API_KEY);
                     Log.e("TestActivity", "Reached getPlaces ");
                     call.enqueue(new Callback<ModelAllResults>() {
@@ -98,56 +98,75 @@ public class DirectionActivity extends AppCompatActivity {
                         public void onResponse(@NonNull Call<ModelAllResults> call, Response<ModelAllResults> response) {
                             if (response.body() != null) {
                                 Log.e("TestActivity", String.valueOf(response.body().getRoute().get(0).getLegs().get(0).getSteps().get(0).getHtmlInstructions()));
-
+                                
                                 ArrayList<ModelSteps> steps = response.body().getRoute().get(0).getLegs().get(0).getSteps();
-                                ArrayList<String> finalMessage = new ArrayList<>();
-
+                                final ArrayList<String> finalMessage = new ArrayList<>();
+                                
                                 ArrayList<String> htmlInstruction = new ArrayList<>();
-                                for ( ModelSteps step: steps)
-                                {
+                                for (int i = 0; i < steps.size(); i++) {
+                                    ModelSteps step = steps.get(i);
                                     htmlInstruction.add(step.getHtmlInstructions());
-                                    Log.e(TAG,  (Html.fromHtml(Html.fromHtml(step.getHtmlInstructions()).toString()))+"\n\n");
-                                    Log.e(TAG, String.valueOf(step.getDistance().get("text"))+"\n");
-
+                                    //Log.e(TAG, (Html.fromHtml(Html.fromHtml(step.getHtmlInstructions()).toString())) + "\n\n");
+                                    //Log.e(TAG, String.valueOf(step.getDistance().get("text")) + "\n");
+                                    
                                     String htmlText = String.valueOf(Html.fromHtml(Html.fromHtml(step.getHtmlInstructions()).toString()));
                                     String distance = String.valueOf(step.getDistance().get("text"));
-
-                                    String finalString = htmlText+" for "+distance+" then";
+                                    
+                                    String finalString = htmlText;
+                                    if (i == steps.size() - 1)
+                                        
+                                        //remove "for" for the last step;
+                                        finalString = htmlText + " for " + distance;
+                                    
+                                    else
+                                        
+                                        finalString = htmlText + " for " + distance + " then";
+                                    
                                     finalMessage.add(finalString);
-
+                                    
                                 }
-
-                                for ( String message : finalMessage)
+                                
+                                for (String message : finalMessage) {
+                                    //Log.e(TAG, message+"\n");
+                                    //SmsHelper.sendDebugSms(String.valueOf(CommonConst.number), SmsHelper.SMS_CONDITION + " " + message);
+                                }
+                                
+                                for(int i =0 ; i < finalMessage.size(); i++)
                                 {
-                                    SmsHelper.sendDebugSms(String.valueOf("9149386335"), SmsHelper.SMS_CONDITION + " "+message);
+                                    Handler handler = new Handler();
+                                    final int finalI = i;
+                                    handler.postDelayed(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            SmsHelper.sendDebugSms(String.valueOf(CommonConst.number),   finalMessage.get(finalI));
+                                        }
+                                    }, 5000*i);
                                 }
-
-                                Log.e(TAG, "The length of steps is "+steps.size());
-
+                                
+                                Log.e(TAG, "The length of steps is " + steps.size());
+                                
                             }
                         }
+                        
                         @Override
                         public void onFailure(Call<ModelAllResults> call, Throwable t) {
                             Log.e("TestActivity", t.toString());
                         }
-
+                        
                     });
-                }
-                else{
-                    for ( String message : predict)
-                    {
-                        SmsHelper.sendDebugSms(String.valueOf("9149386335"), SmsHelper.SMS_CONDITION + "Type in a little more detail\n"+message);
+                } else {
+                    for (String message : predict) {
+                        SmsHelper.sendDebugSms(String.valueOf(CommonConst.number), SmsHelper.SMS_CONDITION + "Type in a little more detail\n" + message);
                     }
                 }
             }
-
+            
             @Override
             public void onFailure(Call<ModelAllResultsAutoPlaces> call, Throwable t) {
-
+            
             }
         });
-
-
+        
         //directions send code
         /*Toast.makeText(getApplicationContext(),startloc+"    "+endloc,Toast.LENGTH_LONG).show();
         Call<ModelAllResults> call = apiService.getAllDirectionData(startloc, endloc, API_KEY);
---
+++ app/src/main/java/com/hackathon/offlinemaps/SmsUtils/CommonConst.java
@@ -0,0 +1,5 @@
+package com.hackathon.offlinemaps.SmsUtils;
+
+public class CommonConst {
+    public static String number = "8765114937";
+}
